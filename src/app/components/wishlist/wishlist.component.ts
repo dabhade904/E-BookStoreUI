dd398f0b6005976c597eed7f8cf0eadeabe4e53f
dabhade904 <dabhade904@gmail.com>
2022-11-30 01:14:11 +0530

    Refactored : to remove inline css

--- src/app/components/wishlist/wishlist.component.ts
+++ src/app/components/wishlist/wishlist.component.ts
@@ -8,22 +8,22 @@ import { WishlistService } from 'src/app/services/wishlist/wishlist.service';
   styleUrls: ['./wishlist.component.scss']
 })
 export class WishlistComponent {
-  wishListItems : any
-  qty : any;
+  wishListItems: any
+  qty: any;
   getWishListBooks = new Array()
-  constructor(private wishListservice : WishlistService, private bookService : GetbooksService) { }
+  constructor(private wishListservice: WishlistService, private bookService: GetbooksService) { }
 
   ngOnInit(): void {
     this.getWishlist()
   }
   getWishlist() {
-    this.wishListservice.getWishlist().subscribe((response : any) => {
+    this.wishListservice.getWishlist().subscribe((response: any) => {
       console.log(response);
       this.wishListItems = response.data
       this.qty = this.wishListItems.length
-      this.wishListItems.forEach((element : any) =>{
+      this.wishListItems.forEach((element: any) => {
         console.log(element)
-        this.bookService.getBookById(element.bookId).subscribe((response : any)=>{
+        this.bookService.getBookById(element.bookId).subscribe((response: any) => {
           console.log(response)
           this.getWishListBooks.push(response.data)
         })
@@ -31,9 +31,9 @@ export class WishlistComponent {
       console.log(this.getWishListBooks)
     })
   }
-  removeFromWishlist(wishListId:any){
-    console.log("bbbbbbbb",wishListId);
-    this.wishListservice.removeFromWishlist(wishListId).subscribe((response : any) =>{
+  removeFromWishlist(wishListId: any) {
+    console.log(wishListId);
+    this.wishListservice.removeFromWishlist(wishListId).subscribe((response: any) => {
       console.log(response);
       this.getWishlist();
     });
